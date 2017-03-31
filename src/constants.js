1333241b6c378d6c2330317ca2cbf5b545bca36a
Stranger in the Q <tripmannn@gmail.com>
2017-03-31 22:07:44 +0300

    init web world wind

---
+++ src/constants.js
@@ -0,0 +1,204 @@
+// see original ../node_modules/web-world-wind/src/WorldWind
+
+module.exports = {
+    /**
+     * The World Wind version number.
+     * @default "0.0.0"
+     * @constant
+     */
+    VERSION: "0.0.0",
+
+    // PLEASE KEEP THE ENTRIES BELOW IN ALPHABETICAL ORDER
+    /**
+     * Indicates an altitude mode relative to the globe's ellipsoid.
+     * @constant
+     */
+    ABSOLUTE: "absolute",
+
+    /**
+     * Indicates that a redraw callback has been called immediately after a redraw.
+     * @constant
+     */
+    AFTER_REDRAW: "afterRedraw",
+
+    /**
+     * Indicates that a redraw callback has been called immediately before a redraw.
+     * @constant
+     */
+    BEFORE_REDRAW: "beforeRedraw",
+
+    /**
+     * The BEGAN gesture recognizer state. Continuous gesture recognizers transition to this state from the
+     * POSSIBLE state when the gesture is first recognized.
+     * @constant
+     */
+    BEGAN: "began",
+
+    /**
+     * The CANCELLED gesture recognizer state. Continuous gesture recognizers may transition to this state from
+     * the BEGAN state or the CHANGED state when the touch events are cancelled.
+     * @constant
+     */
+    CANCELLED: "cancelled",
+
+    /**
+     * The CHANGED gesture recognizer state. Continuous gesture recognizers transition to this state from the
+     * BEGAN state or the CHANGED state, whenever an input event indicates a change in the gesture.
+     * @constant
+     */
+    CHANGED: "changed",
+
+    /**
+     * Indicates an altitude mode always on the terrain.
+     * @constant
+     */
+    CLAMP_TO_GROUND: "clampToGround",
+
+    /**
+     * The radius of Earth.
+     * @constant
+     */
+    EARTH_RADIUS: 6371e3,
+
+    /**
+     * Indicates the cardinal direction east.
+     * @constant
+     */
+    EAST: "east",
+
+    /**
+     * The ENDED gesture recognizer state. Continuous gesture recognizers transition to this state from either
+     * the BEGAN state or the CHANGED state when the current input no longer represents the gesture.
+     * @constant
+     */
+    ENDED: "ended",
+
+    /**
+     * The FAILED gesture recognizer state. Gesture recognizers transition to this state from the POSSIBLE state
+     * when the gesture cannot be recognized given the current input.
+     * @constant
+     */
+    FAILED: "failed",
+
+    /**
+     * Indicates a great circle path.
+     * @constant
+     */
+    GREAT_CIRCLE: "greatCircle",
+
+    /**
+     * Indicates a linear, straight line path.
+     * @constant
+     */
+    LINEAR: "linear",
+
+    /**
+     * Indicates a multi-point shape, typically within a shapefile.
+     */
+    MULTI_POINT: "multiPoint",
+
+    /**
+     * Indicates the cardinal direction north.
+     * @constant
+     */
+    NORTH: "north",
+
+    /**
+     * Indicates a null shape, typically within a shapefile.
+     * @constant
+     */
+    NULL: "null",
+
+    /**
+     * Indicates that the associated parameters are fractional values of the virtual rectangle's width or
+     * height in the range [0, 1], where 0 indicates the rectangle's origin and 1 indicates the corner
+     * opposite its origin.
+     * @constant
+     */
+    OFFSET_FRACTION: "fraction",
+
+    /**
+     * Indicates that the associated parameters are in units of pixels relative to the virtual rectangle's
+     * corner opposite its origin corner.
+     * @constant
+     */
+    OFFSET_INSET_PIXELS: "insetPixels",
+
+    /**
+     * Indicates that the associated parameters are in units of pixels relative to the virtual rectangle's
+     * origin.
+     * @constant
+     */
+    OFFSET_PIXELS: "pixels",
+
+    /**
+     * Indicates a point shape, typically within a shapefile.
+     */
+    POINT: "point",
+
+    /**
+     * Indicates a polyline shape, typically within a shapefile.
+     */
+    POLYLINE: "polyline",
+
+    /**
+     * Indicates a polygon shape, typically within a shapefile.
+     */
+    POLYGON: "polygon",
+
+    /**
+     * The POSSIBLE gesture recognizer state. Gesture recognizers in this state are idle when there is no input
+     * event to evaluate, or are evaluating input events to determine whether or not to transition into another
+     * state.
+     * @constant
+     */
+    POSSIBLE: "possible",
+
+    /**
+     * The RECOGNIZED gesture recognizer state. Discrete gesture recognizers transition to this state from the
+     * POSSIBLE state when the gesture is recognized.
+     * @constant
+     */
+    RECOGNIZED: "recognized",
+
+    /**
+     * The event name of World Wind redraw events.
+     */
+    REDRAW_EVENT_TYPE: "WorldWindRedraw",
+
+    /**
+     * Indicates that the related value is specified relative to the globe.
+     * @constant
+     */
+    RELATIVE_TO_GLOBE: "relativeToGlobe",
+
+    /**
+     * Indicates an altitude mode relative to the terrain.
+     * @constant
+     */
+    RELATIVE_TO_GROUND: "relativeToGround",
+
+    /**
+     * Indicates that the related value is specified relative to the plane of the screen.
+     * @constant
+     */
+    RELATIVE_TO_SCREEN: "relativeToScreen",
+
+    /**
+     * Indicates a rhumb path -- a path of constant bearing.
+     * @constant
+     */
+    RHUMB_LINE: "rhumbLine",
+
+    /**
+     * Indicates the cardinal direction south.
+     * @constant
+     */
+    SOUTH: "south",
+
+    /**
+     * Indicates the cardinal direction west.
+     * @constant
+     */
+    WEST: "west"
+};
